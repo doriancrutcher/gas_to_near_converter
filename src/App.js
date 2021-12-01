a89a546df28ff642a7c13e9d4fb316155f5eb199
Dorian Kinoo Crutcher <doriankinoocrutcher@gmail.com>
2021-12-01 11:01:18 -0800

    included converter from coingecko

--- src/App.js
+++ src/App.js
@@ -21,19 +21,37 @@ function App() {
   const usdRef = useRef();
 
   const [gas, changeGas] = useState("");
-  const [near, changeNEAR] = useState("00");
-  const [usd, changeUSD] = useState("");
+  const [near, changeNEAR] = useState(0.0);
+  const [usd, changeUSD] = useState(0.0);
 
-  const [usdToNearConversion, changeUsdToNearConversion] = useState("");
+  const [nearToUSDConversion, changeNearToUSDConversion] = useState("");
 
-  // conversion caluclators
+  // conversion calculators
+
+  useEffect(() => {
+    const getConversion = async () => {
+      let nearMarketInfo;
+      await fetch(
+        "https://api.coingecko.com/api/v3/coins/near?tickers=true&market_data=true"
+      )
+        .then((response) => response.json())
+        .then((data) => (nearMarketInfo = data));
+      let currentPrice = nearMarketInfo.market_data.current_price.usd;
+      changeNearToUSDConversion(currentPrice);
+    };
+    getConversion();
+  }, []);
 
   // 1 TGAS=1milliseond of compute time = 0.1 milliNEAR = 100 million yoctoNEAR
 
-  const gasToNear = async () => {
+  const gasToNearToUSD = async () => {
     const gasVal = gasRef.current.value;
     changeGas(gasVal);
-    changeNEAR((0.0001 /* NEAR */ / 1) /*TGas */ * gasVal);
+    console.log("changing NEAR");
+    let gasToNEAR = (0.0001 /* NEAR */ / 1) /*TGas */ * gasVal;
+    await changeNEAR(gasToNEAR);
+    console.log("changing USD");
+    await changeUSD(gasToNEAR * nearToUSDConversion);
   };
 
   return (
@@ -51,14 +69,17 @@ function App() {
           <Card style={{ width: "18rem", marginTop: "5vh" }}>
             <Card.Body>
               <Container>
-                <Card.Title>Gas</Card.Title>
-                <Card.Text>Enter your Amount In Gas Here</Card.Text>
+                <Card.Title>Converter</Card.Title>
+                <Card.Text>
+                  Enter your Amount In Gas Here and Hit Submit!
+                </Card.Text>
                 <Form>
                   <Form.Group
                     className='mb-3'
                     controlId='exampleForm.ControlInput1'
                   >
                     <Alert variant={"info"}>{near} NEAR</Alert>
+                    <Alert variant={"info"}>{usd} USD</Alert>
                     <Form.Label>Amount </Form.Label>
                     <Form.Control
                       ref={gasRef}
@@ -67,7 +88,12 @@ function App() {
                   </Form.Group>
                 </Form>
 
-                <Button variant='primary' onClick={gasToNear}>
+                <Button
+                  variant='primary'
+                  onClick={async () => {
+                    await gasToNearToUSD();
+                  }}
+                >
                   Submit
                 </Button>
               </Container>
